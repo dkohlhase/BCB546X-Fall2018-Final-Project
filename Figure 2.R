531c71b8eb68b5b3961af16d9194f75026d0ca12
Cassandra Winn <cassandradohse@cwinnmac.student.iastate.edu>
2018-11-16 11:26:09 -0600

    Edit code

--- Figure 2.R
+++ Figure 2.R
@@ -1,5 +1,22 @@
-library(xlsx)
 setwd("../Romay_et_al_2013")
-IBSdata <- read.csv("Additional_File_2.csv", header = TRUE)
+IBSdata <- read.csv("IBS_Pairs.csv", header = TRUE)
+nrow(IBSdata)
 
-x <- data.frame("Pairs"=NA, "IBS"=NA)
+length(unique(IBSdata$Pairs))
+#this ensures that all pairs are unique
+
+
+#Figure A
+library(ggplot2)
+ggplot(IBSdata, aes(x=IBS)) + geom_histogram(bins = 20, color="black", fill="grey") + 
+  ggtitle("Distribution of IBS Relationships Among Pairs of Closely Related Lines") +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  ylab("Number of Pairs")
+
+#Subset of Figure A
+IBSdata_Filter <- IBSdata[ which(IBSdata$IBS >=0.95), ]
+library(ggplot2)
+ggplot(IBSdata_Filter, aes(x=IBS)) + geom_histogram(bins=5, color="black", fill="grey") + 
+  ggtitle("Distribution of IBS Relationships Among Pairs of Closely Related Lines") +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  ylab("Number of Pairs")
