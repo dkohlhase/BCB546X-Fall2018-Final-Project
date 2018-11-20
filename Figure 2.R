62193d2cc7bba1c44bb9140e7d9f6c43de8151be
Cassandra Winn <cassandradohse@cwinnmac.student.iastate.edu>
2018-11-20 12:34:04 -0600

    Fig 2

--- Figure 2.R
+++ Figure 2.R
@@ -1,5 +1,9 @@
 setwd("../Romay_et_al_2013")
 IBSdata <- read.csv("IBS_Pairs.csv", header = TRUE)
+#this data set was made in excel from additional file 2, need to find a way to create data set in R
+# or, calculate IBS as proportion of markers (across all 10 chromosomes) identical by state for all pairs of lines (4,351) 
+
+
 nrow(IBSdata)
 
 length(unique(IBSdata$Pairs))
