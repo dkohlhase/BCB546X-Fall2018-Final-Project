4be7a4c3b22e988d31b303acff06e11a7dd261df
Cassandra Winn <cassandradohse@cwinnmac.student.iastate.edu>
2018-11-29 11:29:41 -0600

    updates

--- Figure_2.R
+++ Figure_2.R
@@ -24,3 +24,36 @@ ggplot(IBSdata_Filter, aes(x=IBS)) + geom_histogram(bins=5, color="black", fill=
   ggtitle("Distribution of IBS Relationships Among Pairs of Closely Related Lines") +
   theme(plot.title = element_text(hjust = 0.5)) +
   ylab("Number of Pairs")
+
+
+
+#-------------------------------------------------------
+##Figure 2 update with all IBS data
+
+m1 <- read.csv("", header=TRUE)
+library(reshape2)
+IBS <- setNames(melt(m1), c('line1', 'line2', 'IBS'))
+
+library(dplyr)
+IBSdata <- unite_(IBS, "line1_line2", c("line1","line2"))
+
+nrow(IBSdata)
+
+length(unique(IBSdata$line1_line2))
+#this ensures that all pairs are unique
+
+
+#Figure A
+library(ggplot2)
+ggplot(IBSdata, aes(x=IBS)) + geom_histogram(bins = 20, color="black", fill="grey") + 
+  ggtitle("Distribution of IBS Relationships Among Pairs of Closely Related Lines") +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  ylab("Number of Pairs")
+
+#Subset of Figure A
+IBSdata_Filter <- IBSdata[ which(IBSdata$IBS >=0.95), ]
+library(ggplot2)
+ggplot(IBSdata_Filter, aes(x=IBS)) + geom_histogram(bins=5, color="black", fill="grey") + 
+  ggtitle("Distribution of IBS Relationships Among Pairs of Closely Related Lines") +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  ylab("Number of Pairs")
