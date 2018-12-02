86bdc9c8a0829e312163d8ad7717f5bce7a3a42e
Cassandra Winn <cassandradohse@Cassandras-MacBook-Pro.local>
2018-12-02 12:54:54 -0600

    changed scale of graph from # of SNPs to % SNPs

--- Figure_5.R
+++ Figure_5.R
@@ -12,7 +12,9 @@ allchr <- rbind(chr1, chr2, chr3, chr4, chr5, chr6, chr7, chr8, chr9, chr10)
 allchr_filter <- allchr[1:15]
 
 library(ggplot2)
-ggplot(allchr_filter, aes(x=Minor.Allele.Frequency)) + geom_histogram(bins=50,color="turquoise", fill="turquoise") + 
+ggplot(allchr_filter, aes(x=Minor.Allele.Proportion)) + geom_histogram(bins=50,color="turquoise", fill="turquoise") + 
   ggtitle("Minor Allele Frequency Across SNPs") +
-  theme(plot.title = element_text(hjust = 0.5)) +
-  ylab("Number of SNPs")
+  theme(plot.title = element_text(hjust = 0.5)) +  scale_y_continuous(labels = function(x) paste(x/10000, "%")) +
+  scale_x_continuous(breaks=seq(0.0,0.50,0.02)) + theme(axis.text.x = element_text(angle = 90, hjust = 1)) + 
+  ylab("% SNPs") +
+  xlab("Minor Allele Frequency")
