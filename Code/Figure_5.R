1517b41768ed047352ffddb15a1d53cad6f22975
Cassandra Winn <cassandradohse@Cassandras-MacBook-Pro.local>
2018-12-03 21:22:48 -0600

    Commented code

--- Code/Figure_5.R
+++ Code/Figure_5.R
@@ -1,20 +1,35 @@
-setwd("../TASSEL")
+# set working directory to folder where MAF data from TASSEL is stored
+setwd("./Data/TASSEL")
 
+#read in all files that fit the pattern "Chr*_MAF.csv" into a list of dataframes
 dataFiles <- lapply(Sys.glob("Chr*_MAF.csv"), read.csv)
+
+#give these files names so we can refer to them in the for statement below
 names(dataFiles) <- c("10", "1", "2", "3", "4", "5", "6", "7", "8", "9")
 
+#for all i dataframes in list dataFiles, rename them as chr[[i]]
 for(i in names(dataFiles)){
   d.frame <-
     assign(paste("chr", i, sep=""), dataFiles[[i]])
 }
 
+# bind all dataframes together
 allchr <- rbind(chr1, chr2, chr3, chr4, chr5, chr6, chr7, chr8, chr9, chr10)
-allchr_filter <- allchr[1:15]
 
+#graph data in histogram: % SNPs vs MAF
 library(ggplot2)
-ggplot(allchr_filter, aes(x=Minor.Allele.Proportion)) + geom_histogram(bins=50,color="turquoise", fill="turquoise") + 
+ggplot(allchr, aes(x=Minor.Allele.Frequency)) + geom_histogram(bins=40,color="cyan3", fill="cyan3") + 
   ggtitle("Minor Allele Frequency Across SNPs") +
-  theme(plot.title = element_text(hjust = 0.5)) +  scale_y_continuous(labels = function(x) paste(x/10000, "%")) +
-  scale_x_continuous(breaks=seq(0.0,0.50,0.02)) + theme(axis.text.x = element_text(angle = 90, hjust = 1)) + 
+  scale_y_continuous(expand = c(0,0), labels = function(x) paste(x/10000)) +
+  scale_x_continuous(expand = c(0,0), breaks=seq(0.00,0.50,0.02)) +
   ylab("% SNPs") +
-  xlab("Minor Allele Frequency")
+  xlab("Minor Allele Frequency") +
+  theme_bw() + #this removes grey background from graph
+  theme( 
+    panel.grid.major.x = element_blank() , #remove major verticle grid lines
+    panel.grid.minor.x = element_blank() , #remove minor verticle grid lines
+    panel.grid.minor.y = element_blank() , #remove minor horizontal grid lines
+    panel.grid.major.y = element_line( size=.1, color="black" ) , #set the horizontal major grid lines so they don't disappear
+    axis.text.x = element_text(angle = 90, hjust = 1) , # x axis labels set to 90 degrees
+    plot.title = element_text(hjust = 0.5) #center the title of the graph
+  )
